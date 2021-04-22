a630a35c6df8a55025a4d6400a11450a3e69277b
Coleen <79562228+cstuhlfire@users.noreply.github.com>
2021-04-22 02:37:23 -0500

    Added GET for /api/categories

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -3,6 +3,9 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
+
+// get all products
+// including associated Category and Tag data
 router.get('/', async (req, res) => {
   try {
     const productData = await Product.findAll({
@@ -18,12 +21,6 @@ router.get('/', async (req, res) => {
       res.status(500).json(err);
     }
 });
-
-// get all products
-//router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
-//});
 
 // get one product
 router.get('/:id', (req, res) => {
