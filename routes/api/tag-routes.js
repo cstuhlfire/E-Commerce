5c83ea22e00fa4da22015c01cc3f7b889cac092d
Coleen <79562228+cstuhlfire@users.noreply.github.com>
2021-04-22 16:07:28 -0500

    Added route for /api/tags/:id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -7,20 +7,38 @@ const { Tag, Product, ProductTag } = require('../../models');
 // be sure to include its associated Product data
 router.get('/', async (req, res) => {
   try {
-    const productData = await Tag.findAll({
+    const tagtData = await Tag.findAll({
       include: [{ model: Product, through: ProductTag }]
     });
 
-    res.status(200).json(productData);
+    res.status(200).json(tagData);
   } 
     catch (err) {
       res.status(500).json(err);
     }
 });
 
-router.get('/:id', (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  // including its associated Product data
+router.get('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include:[{model: Product, through: ProductTag}]
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', (req, res) => {
+
 });
 
 router.post('/', (req, res) => {
