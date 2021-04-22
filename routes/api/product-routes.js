02b024cf4b1952482834669d134edef87556d6a7
Coleen <79562228+cstuhlfire@users.noreply.github.com>
2021-04-22 15:55:29 -0500

    Added routes for /api/products/:id and /api/categories/:id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -22,10 +22,27 @@ router.get('/', async (req, res) => {
     }
 });
 
-// get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+// get one product - find a single product by its `id`
+// including associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      // JOIN with Category and Tag through ProductTag
+      include:[
+        {model: Category},
+        {model: Tag, through: ProductTag}
+      ]
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
